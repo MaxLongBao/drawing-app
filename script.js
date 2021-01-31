df7e3150a1af75073b53c3162ba439435c7dcdf0
Massimiliano Bisceglie <maxbisceglie89@gmail.com>
2021-01-31 12:32:06 +0000

    test touchscreen

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ console.log('hello');
 const canvas = document.getElementById('canvas');
 canvas.height = 550;
 canvas.width = 1200;
+// canvas.height = 55;
+// canvas.width = 120;
 const ctx = canvas.getContext('2d');
 
 let painting = false;
@@ -22,11 +24,20 @@ const draw = (e) => {
   ctx.lineWidth = 10;
   ctx.lineCap = 'round';
   ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.lineTo((e.touches[0].clientX - e.target.offsetLeft), (e.touches[0].clientY - e.target.offsetTop));
   ctx.stroke();
-
+  e.preventDefault();
 }
 
-canvas.addEventListener('mousedown', startPosition);
-canvas.addEventListener('mouseup', endPosition);
-canvas.addEventListener('mouseleave', endPosition);
-canvas.addEventListener('mousemove', draw);
+const hello = () => {
+  console.log('hello');
+}
+
+// canvas.addEventListener('mousedown', startPosition);
+// canvas.addEventListener('mouseup', endPosition);
+// canvas.addEventListener('mouseleave', endPosition);
+// canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('touchstart', startPosition);
+canvas.addEventListener('touchstart', hello);
+canvas.addEventListener('touchend', endPosition);
+canvas.addEventListener('touchmove', draw);
